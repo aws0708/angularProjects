0e467a8c0cd717f0c6879dccbc3bf81ce876397b
Anubhaw-Singh <vkt94594@gmail.com>
2026-01-02 15:50:33 +0530

    completed booking section for the app

--- Car-Rental-App-Angular-20/src/app/pages/booking/booking.ts
+++ Car-Rental-App-Angular-20/src/app/pages/booking/booking.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { BookingService } from '../../service/booking';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { APIResponse } from '../../model/car';
 
 @Component({
   selector: 'app-booking',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './booking.html',
   styleUrl: './booking.css',
 })
-export class Booking {
+export class Booking implements OnInit {
+  bookingSrv = inject(BookingService);
+  bookingForm: FormGroup = new FormGroup({
+    customerName: new FormControl(''),
+    customerCity: new FormControl(''),
+    mobileNo: new FormControl(''),
+    email: new FormControl(''),
+    bookingId: new FormControl(0),
+    carId: new FormControl(''),
+    bookingDate: new FormControl(''),
+    discount: new FormControl(0),
+    totalBillAmount: new FormControl(0),
+  })
+  carList: any[] = [];
+  bookingList: any[] = []
 
+  ngOnInit(): void {
+    this.getBookingList();
+    this.getCarList();
+    
+  }
+
+  getCarList(){
+    this.bookingSrv.getAllCars().subscribe((res: APIResponse) => {
+      this.carList = res.data;
+    });
+  }
+  getBookingList(){
+    this.bookingSrv.getAllBookings().subscribe((res: APIResponse) => {
+      this.bookingList = res.data;
+    })
+  }
+  saveBooking() {
+    const bookingFormVal = this.bookingForm.value;
+    this.bookingSrv.saveNewBooking(bookingFormVal).subscribe((res:any)=>{
+      if(res.result){
+        alert(res.message);
+        this.getBookingList();
+      }
+    });
+  }
 }
---
+++ Car-Rental-App-Angular-20/src/app/service/booking.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { APIResponse } from '../model/car';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookingService {
+  constructor(private http: HttpClient) { }
+  apiUrl: string = "https://freeapi.miniprojectideas.com/api/CarRentalApp/";
+  getAllBookings() {
+    return this.http.get<APIResponse>(this.apiUrl + 'geAllBookings');
+  }
+  getAllCars() {
+    return this.http.get<APIResponse>(this.apiUrl + 'GetCars');
+  }
+  saveNewBooking(obj: any) {
+    return this.http.post(this.apiUrl + "CreateNewBooking", obj)
+  }
+
+}
